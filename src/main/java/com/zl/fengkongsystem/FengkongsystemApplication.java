fec82353f60710a1107678531848f33c5229cfd4
daideking <41370135+daideking@users.noreply.github.com>
2018-11-08 09:37:11 +0800

    First Verson

---
+++ src/main/java/com/zl/fengkongsystem/FengkongsystemApplication.java
@@ -0,0 +1,12 @@
+package com.zl.fengkongsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FengkongsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FengkongsystemApplication.class, args);
+	}
+}
